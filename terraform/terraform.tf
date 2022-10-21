c0eb25e8344526493f02861a736a93d0d9771a8c
Kevin Cheng <>
2022-10-21 10:35:31 +0800

    change to s3 remote state

---
+++ terraform/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-openapi-petstore"
+    key = "terraform.tfstate"
+    region = "ap-southeast-1"
+    dynamodb_table = "terraform-state-locking-openapi-petstore"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.34"
+    }
+  }
+
+  required_version = ">= 1.3.2"
+}
